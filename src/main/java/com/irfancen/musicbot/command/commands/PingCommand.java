9ee7b7e6c1ff567fe5b49cc8117d0780c370ef29
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-19 23:32:07 +0700

    Add Ping Command

---
+++ src/main/java/com/irfancen/musicbot/command/commands/PingCommand.java
@@ -0,0 +1,23 @@
+package com.irfancen.musicbot.command.commands;
+
+import com.irfancen.musicbot.command.CommandContext;
+import com.irfancen.musicbot.command.ICommand;
+import net.dv8tion.jda.api.JDA;
+
+public class PingCommand implements ICommand {
+
+    @Override
+    public void handle(CommandContext ctx) {
+        JDA jda = ctx.getJDA();
+
+        jda.getRestPing().queue(
+                (ping) -> ctx.getChannel()
+                        .sendMessageFormat(":ping_pong: REST: %sms | WS: %sms", ping, jda.getGatewayPing()).queue()
+        );
+    }
+
+    @Override
+    public String getName() {
+        return "ping";
+    }
+}

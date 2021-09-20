104d72e10ba6e6c2181783fecc804e47be0108e7
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-20 18:09:59 +0700

    Gateway Intents Setup

--- src/main/java/com/irfancen/musicbot/Bot.java
+++ src/main/java/com/irfancen/musicbot/Bot.java
@@ -4,6 +4,8 @@ import com.irfancen.musicbot.database.SQLiteDataSource;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 
 public class Bot {
@@ -12,7 +14,18 @@ public class Bot {
     {
         SQLiteDataSource.getConnection();
 
-        JDA api = JDABuilder.createDefault(Config.get("bot_token"))
+        JDA api = JDABuilder.createDefault(
+                Config.get("bot_token"),
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS
+        )
+                .disableCache(
+                        CacheFlag.CLIENT_STATUS,
+                        CacheFlag.ACTIVITY,
+                        CacheFlag.EMOTE
+                )
                 .addEventListeners(new BotListener())
                 .setActivity(Activity.playing("with her Developer."))
                 .build().awaitReady();

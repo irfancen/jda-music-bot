5a6f57ceb401f45e7577fe080dbd19f26139901f
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-21 10:34:23 +0700

    Add Stop Command

--- src/main/java/com/irfancen/musicbot/CommandManager.java
+++ src/main/java/com/irfancen/musicbot/CommandManager.java
@@ -7,6 +7,7 @@ import com.irfancen.musicbot.command.commands.PingCommand;
 import com.irfancen.musicbot.command.commands.admin.SetPrefixCommand;
 import com.irfancen.musicbot.command.commands.music.JoinCommand;
 import com.irfancen.musicbot.command.commands.music.PlayCommand;
+import com.irfancen.musicbot.command.commands.music.StopCommand;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 import javax.annotation.Nullable;
@@ -25,6 +26,7 @@ public class CommandManager {
         // Music Commands
         addCommand(new JoinCommand());
         addCommand(new PlayCommand());
+        addCommand(new StopCommand());
     }
 
     private void addCommand(ICommand cmd) {

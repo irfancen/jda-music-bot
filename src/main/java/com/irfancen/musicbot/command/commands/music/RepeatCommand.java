5282094fe68d13714a3719d05127976395306e3c
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-22 22:31:44 +0700

    Add Repeat Command

---
+++ src/main/java/com/irfancen/musicbot/command/commands/music/RepeatCommand.java
@@ -0,0 +1,62 @@
+package com.irfancen.musicbot.command.commands.music;
+
+import com.irfancen.musicbot.command.CommandContext;
+import com.irfancen.musicbot.command.ICommand;
+import com.irfancen.musicbot.lavaplayer.GuildMusicManager;
+import com.irfancen.musicbot.lavaplayer.PlayerManager;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.List;
+
+public class RepeatCommand implements ICommand {
+    @Override
+    public void handle(CommandContext ctx) {
+        final TextChannel channel = ctx.getChannel();
+        final Member self = ctx.getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (!selfVoiceState.inVoiceChannel()) {
+            channel.sendMessage("I need to be in a voice channel for this to work").queue();
+            return;
+        }
+
+        final Member member = ctx.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+        if (!memberVoiceState.inVoiceChannel()) {
+            channel.sendMessage("You need to be in the voice channel for this to work.").queue();
+            return;
+        }
+
+        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+            channel.sendMessage("You need to be in the same voice channel as me for this to work.").queue();
+            return;
+        }
+
+        final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
+        final boolean newRepeating = !musicManager.scheduler.repeating;
+
+        musicManager.scheduler.repeating = newRepeating;
+
+        channel.sendMessage(newRepeating ? "Repeating current song." : "No longer repeating current song.").queue();
+    }
+
+    @Override
+    public String getName() {
+        return "repeat";
+    }
+
+    @Override
+    public String getHelp(String prefix) {
+        return String.format("**Loops the current song**\n" +
+                "Usage: `%1$srepeat`\n" +
+                "Aliases: `%1$sloop`", prefix);
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("loop");
+    }
+}

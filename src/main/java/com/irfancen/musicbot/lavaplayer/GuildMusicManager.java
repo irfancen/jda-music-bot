a6830ac32afc9dfb328e2e71711a7179a422a2bd
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-28 09:55:39 +0700

    Disconnect after inactivity

--- src/main/java/com/irfancen/musicbot/lavaplayer/GuildMusicManager.java
+++ src/main/java/com/irfancen/musicbot/lavaplayer/GuildMusicManager.java
@@ -2,15 +2,18 @@ package com.irfancen.musicbot.lavaplayer;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import net.dv8tion.jda.api.entities.Guild;
 
 public class GuildMusicManager {
     public final AudioPlayer audioPlayer;
     public final TrackScheduler scheduler;
+    public final Guild guild;
     private final AudioPlayerSendHandler sendHandler;
 
-    public GuildMusicManager (AudioPlayerManager manager) {
+    public GuildMusicManager (AudioPlayerManager manager, Guild guild) {
         this.audioPlayer = manager.createPlayer();
-        this.scheduler = new TrackScheduler(this.audioPlayer);
+        this.guild = guild;
+        this.scheduler = new TrackScheduler(this.audioPlayer, guild);
         this.audioPlayer.addListener(this.scheduler);
         this.sendHandler = new AudioPlayerSendHandler(this.audioPlayer);
     }
--- src/main/java/com/irfancen/musicbot/lavaplayer/TrackScheduler.java
+++ src/main/java/com/irfancen/musicbot/lavaplayer/TrackScheduler.java
@@ -4,18 +4,25 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TrackScheduler extends AudioEventAdapter {
     public final AudioPlayer player;
     public final BlockingQueue<AudioTrack> queue;
     public boolean repeating = false;
+    public final Guild guild;
 
-    public TrackScheduler(AudioPlayer player) {
+    public TrackScheduler(AudioPlayer player, Guild guild) {
         this.player = player;
         this.queue = new LinkedBlockingQueue<>();
+        this.guild = guild;
     }
 
     public void queue(AudioTrack track) {
@@ -30,6 +37,18 @@ public class TrackScheduler extends AudioEventAdapter {
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if (queue.isEmpty()) {
+            Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+                //Only disconnect if nothing has been played during the time.
+                if (player.getPlayingTrack() == null) {
+                    this.player.startTrack(null, false);
+
+                    AudioManager audioManager = this.guild.getAudioManager();
+                    audioManager.closeAudioConnection();
+                }
+            }, 5, TimeUnit.MINUTES);
+        }
+
         if (endReason.mayStartNext) {
             if (this.repeating) {
                 this.player.startTrack(track.makeClone(), false);

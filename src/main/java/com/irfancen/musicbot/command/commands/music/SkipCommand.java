fd52f258a5d4ccc003879cf9e97db5d4a378794a
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-09-28 23:52:39 +0700

    Skip to index of song in queue

--- src/main/java/com/irfancen/musicbot/command/commands/music/SkipCommand.java
+++ src/main/java/com/irfancen/musicbot/command/commands/music/SkipCommand.java
@@ -5,12 +5,15 @@ import com.irfancen.musicbot.command.ICommand;
 import com.irfancen.musicbot.lavaplayer.GuildMusicManager;
 import com.irfancen.musicbot.lavaplayer.PlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
 public class SkipCommand implements ICommand {
     @Override
@@ -61,12 +64,47 @@ public class SkipCommand implements ICommand {
             return;
         }
 
-        musicManager.scheduler.nextTrack();
-        channel.sendMessage(new EmbedBuilder()
-                .setDescription("Skipped to the next song")
-                .build())
-                .queue();
+        final BlockingQueue<AudioTrack> queue = musicManager.scheduler.queue;
+        final EmbedBuilder removeEmbed = new EmbedBuilder();
+
+        try {
+            if (ctx.getArgs().isEmpty()) {
+                musicManager.scheduler.nextTrack();
+                channel.sendMessage(new EmbedBuilder()
+                        .setDescription("Skipped to the next song")
+                        .build())
+                        .queue();
+                return;
+            }
+            int index = Integer.parseInt(ctx.getArgs().get(0));
+            AudioTrack song = List.copyOf(queue).get(index - 1);
+            for (int i = 0; i < index - 1; i++) {
+                queue.poll();
+            }
+            musicManager.scheduler.nextTrack();
+
+            channel.sendMessage(
+                    removeEmbed.setDescription(
+                            String.format("Skipped to [%s](%s)", song.getInfo().title, song.getInfo().uri))
+                            .build())
+                    .queue();
+        } catch (NumberFormatException e) {
+            channel.sendMessage(
+                    removeEmbed.setDescription("Argument needs to be the index of the queue")
+                            .setColor(Color.RED)
+                            .build())
+                    .queue();
+        } catch (IndexOutOfBoundsException e) {
+            channel.sendMessage(
+                    removeEmbed.setDescription(
+                            String.format("There are no songs at index **%d** of the queue", Integer.parseInt(ctx.getArgs().get(0))))
+                            .setColor(Color.RED)
+                            .build())
+                    .queue();
+        }
     }
+
+
 
     @Override
     public String getName() {
@@ -76,6 +114,6 @@ public class SkipCommand implements ICommand {
     @Override
     public String getHelp(String prefix) {
         return String.format("**Skips the current song**\n" +
-                "Usage: `%1$sskip`\n", prefix);
+                "Usage: `%1$sskip` or `%1$sskip <index of song>`\n", prefix);
     }
 }

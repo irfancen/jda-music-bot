118f4b40a42f2247eca7aeccff01fc84d52ace38
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2023-04-11 20:22:58 +0700

    Update dependency and dotenv implementation

--- src/main/java/com/irfancen/musicbot/Config.java
+++ src/main/java/com/irfancen/musicbot/Config.java
@@ -3,7 +3,10 @@ package com.irfancen.musicbot;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+    private Config(){}
 
     public static String get(String key) {
         return dotenv.get(key.toUpperCase());
